ec398837956ecaa87e6e185b521730d66a7330d2
slimbook <dev@slimbook.es>
2021-06-17 12:03:42 +0200

    Version 0.3.3 Upgrade

--- debian/slimbookrgbkeyboard/usr/share/slimbookrgbkeyboard/src/custom_conf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-tee /etc/modprobe.d/clevo-xsm-wmi.conf <<< 'options clevo-xsm-wmi kb_color=white,white,white, kb_brightness=10'
-
-tee -a /etc/modprobe.d/clevo-xsm-wmi.conf <<< '#last_color=white'
