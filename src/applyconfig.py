fc63febf9a9d2a51f100d811c2b5c6ae2c08f4ed
slimbook <dev@slimbook.es>
2021-06-23 17:41:47 +0200

    Version 0.3.4

--- src/applyconfig.py
+++ src/applyconfig.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-#unused yet but it will go on .autostrart
+#triggered by .autostrart
 import os
 import sys
 import subprocess
 from configparser import ConfigParser
 
-USER_NAME = subprocess.getoutput("logname")
+USER_NAME = subprocess.getoutput('last -wn1 | head -n 1 | cut -f 1 -d " "')
 print("Username: "+USER_NAME)
 
 HOMEDIR = subprocess.getoutput("echo ~"+USER_NAME)
 print("Homedir: "+HOMEDIR+"\n")
 
+
 config_file = HOMEDIR+'/.config/slimbookrgbkeyboard/slimbookrgbkeyboard.conf'
+print("---- "+config_file+"\n")
 
 config = ConfigParser()
 config.read(config_file)
@@ -23,7 +25,7 @@ os.system("cat "+config_file)
 def main(args):
 
     #print("Total Args --> "+ str(len(args)) +" --> arg[0] = "+args[0])
-
+    print(str(args))
     # Argument 0 is the current route
     if len(args) > 1:
         print("Setting "+args[1]+" ...")
@@ -63,20 +65,9 @@ def main(args):
             call = subprocess.getstatusoutput("sudo su -c 'echo "+lb_color+" > /sys/class/leds/qc71_laptop\:\:lightbar/color'")
             print("Exit: "+str(call[0]))#Apply color
 
-    else:
         
-        if config.get('CONFIGURATION', 'state'):
-            state = config.get('CONFIGURATION', 'state')
-            effect = config.get('CONFIGURATION', 'effect')
-            brightness = config.get('CONFIGURATION', 'brightness')
-            suspension = config.get('CONFIGURATION', 'suspension')
-
-            lb_state = config.get('CONFIGURATION', 'lb_state')
-            lb_rainbow = config.get('CONFIGURATION', 'lb_rainbow')
-            lb_color = config.get('CONFIGURATION', 'lb_color')
-        
-
-        if suspension=="pre":
+        elif args[1]  == "pre":
+            print('Suspending ...')
             os.system("sudo ite8291r3-ctl off")  #Turn off keyboard
             call = subprocess.getstatusoutput("sudo su -c 'echo 0 > /sys/class/leds/qc71_laptop\:\:lightbar/brightness'")
             print(str(call))#Apply 
@@ -88,37 +79,56 @@ def main(args):
             with open(config_file, 'w') as configfile:
                 config.write(configfile)
 
-        else:
-            if suspension=="post" or suspension=="":
-                # We apply the config
+        elif args[1] == "post":
+
+
+            if config.get('CONFIGURATION', 'state'):
+
+                state = config.get('CONFIGURATION', 'state')
+                effect = config.get('CONFIGURATION', 'effect')
+                brightness = config.get('CONFIGURATION', 'brightness')
+                suspension = config.get('CONFIGURATION', 'suspension')
+
+                lb_state = config.get('CONFIGURATION', 'lb_state')
+                lb_rainbow = config.get('CONFIGURATION', 'lb_rainbow')
+                lb_color = config.get('CONFIGURATION', 'lb_color')
+
+                # We apply the keyboard config
                 if state=="on":
+                    print(state)
                     os.system(effect)
-                    os.system("sudo ite8291r3-ctl brightness "+brightness)   
-                    
+                    call = subprocess.getstatusoutput(effect)   
+                    print("Exit: "+str(call))#Apply state
+
+                    call = subprocess.getstatusoutput("sudo ite8291r3-ctl brightness "+brightness)   
+                    print("Exit: "+str(call))#Apply state
+
                 else:
                     os.system("sudo ite8291r3-ctl off")  
 
-                # We apply the config
+                # We apply the lightbar config
                 if lb_state == "1":
                     print("Aplicando cambios lightbar")    
 
                     print("Aplicando color "+lb_state)            	
                     call = subprocess.getstatusoutput("sudo su -c 'echo "+lb_state+" > /sys/class/leds/qc71_laptop\:\:lightbar/brightness'")
-                    print("Exit: "+str(call[0]))#Apply state        	
+                    #print("Exit: "+str(call[0]))#Apply state   
+                    print("Exit: "+str(call))#Apply state       	
                     
                     print("Aplicando rainbow "+lb_rainbow)
                     call = subprocess.getstatusoutput("sudo su -c 'echo "+lb_rainbow+" > /sys/class/leds/qc71_laptop\:\:lightbar/rainbow_mode'")
-                    print("Exit: "+str(call[0]))#Apply rainbow
+                    #print("Exit: "+str(call[0]))#Apply rainbow
+                    print("Exit: "+str(call))#Apply state      
                     
                     print("Aplicando color "+lb_color)            	
                     call = subprocess.getstatusoutput("sudo su -c 'echo "+lb_color+" > /sys/class/leds/qc71_laptop\:\:lightbar/color'")
-                    print("Exit: "+str(call[0]))#Apply color
+                    #print("Exit: "+str(call[0]))#Apply color
+                    print("Exit: "+str(call))#Apply state      
                     
                 else:
                     os.system("echo 0 > /sys/class/leds/qc71_laptop\:\:lightbar/brightness")  #Turn off light bar 
-            
-        
-os.system("cat "+config_file)
+               
+    
 
 if __name__ == "__main__":
     #Se obtiene las variables que se le pasa desde el archivo /usr/share/slimbookface/slimbookface
